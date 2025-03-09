0bd021d006488cfec4313a66dd85bdb4d6295938
SOLLodkiy <makssoll245@gmail.com>
2025-03-09 18:17:12 +0500

    ok

--- movie.js
+++ movie.js
@@ -1,38 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
     const movieInfoContainer = document.getElementById("movie-info");
 
-    // Получаем название фильма из URL
-    const pathParts = window.location.pathname.split("/");
-    const movieName = decodeURIComponent(pathParts[pathParts.length - 1]);
+    // Получаем название фильма из URL (query-параметры)
+    const urlParams = new URLSearchParams(window.location.search);
+    const movieName = decodeURIComponent(urlParams.get("name"));
 
     // Данные о фильмах
     const moviesData = {
-        "Дюна: Часть вторая": {
+        "дюна-часть-вторая": {
+            title: "Дюна: Часть вторая",
             poster: "https://ru-images.kinorium.com/movie/300/2808886.jpg?1736334495",
             description: "Продолжение эпической саги о Пола Атрейдесе.",
             rating: 8.9
         },
-        "Оппенгеймер": {
+        "оппенгеймер": {
+            title: "Оппенгеймер",
             poster: "https://ru-images.kinorium.com/movie/300/2828885.jpg?1690229381",
             description: "История создания атомной бомбы.",
             rating: 9.2
         },
-        "Барби": {
+        "барби": {
+            title: "Барби",
             poster: "https://ru-images-s.kinorium.com/movie/300/501616.jpg?1694458556",
             description: "История Барби в реальном мире.",
             rating: 7.8
         },
-        "Аватар: Путь воды": {
+        "аватар-путь-воды": {
+            title: "Аватар: Путь воды",
             poster: "https://ru-images-s.kinorium.com/movie/300/528138.jpg?1683110297",
             description: "Продолжение истории на Пандоре.",
             rating: 8.2
         },
-        "Человек-паук: Через вселенные": {
+        "человекпаук-через-вселенные": {
+            title: "Человек-паук: Через вселенные",
             poster: "https://ru-images.kinorium.com/movie/300/1446885.jpg?1654075889",
             description: "Майлз Моралес отправляется в мультивселенную.",
             rating: 8.7
         },
-        "Соник 3": {
+        "соник-3": {
+            title: "Соник 3",
             poster: "https://ru-images-s.kinorium.com/movie/300/8663323.jpg?1731588344",
             description: "Анимационный фильм про Соника.",
             rating: 7.5
@@ -56,8 +62,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Генерируем HTML-код для отображения фильма
         movieInfoContainer.innerHTML = `
-            <h1>${movieName}</h1>
-            <img src="${movie.poster}" alt="${movieName}" class="movie-poster">
+            <h1>${movie.title}</h1>
+            <img src="${movie.poster}" alt="${movie.title}" class="movie-poster">
             <p>${movie.description}</p>
             <p><strong>Рейтинг:</strong> ${movie.rating}</p>
             <p><strong>Зал:</strong> ${savedHall}</p>
